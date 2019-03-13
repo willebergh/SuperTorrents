b4d980172621955a328b6709b4e65c266cebbe60
willebergh <48175538+willebergh@users.noreply.github.com>
2019-03-13 18:36:33 +0100

    optimized home page styling for phones and other small devices

--- home/index.php
+++ home/index.php
@@ -6,70 +6,68 @@
 ?>
   <style media="screen">.headerNav_liHome a {background-color: #2d98da;}</style>
   <main class="main">
-    <div class="frontPageArticleWrapper">
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Torrents right now</h5>
-          </div>
-        </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Torrents right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
           $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Movies right now</h5>
-          </div>
-        </div>
+    </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Movies right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
           $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'movies'  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Tv-shows right now</h5>
-          </div>
-        </div>
+    </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Tv-shows right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
           $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'tv-shows'  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Music right now</h5>
-          </div>
-        </div>
+    </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Music right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
           $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'music'  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Games right now</h5>
-          </div>
-        </div>
+    </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Games right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
           $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'games'  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
-      <div class="frontPageArticle">
-        <div class="tableContainer">
-          <div class="browseTorrent_tableHeader">
-            <h5>Most popular Software right now</h5>
-          </div>
-        </div>
+    </div>
+    <div class="homePage_onDisplay">
+      <div class="homePage_onDisplay_title">
+        <h5>Most popular Software right now</h5>
+      </div>
+      <div class="homePage_onDisplay_content">
         <?php
-          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'tv-shows'  ORDER BY seeders DESC LIMIT 10";
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'software'  ORDER BY seeders DESC LIMIT 10";
           require($browseTorrent_tableINC);
         ?>
       </div>
