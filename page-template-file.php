f94f52849e541dc8336d3781cd4c5549b04520b3
willebergh <48175538+willebergh@users.noreply.github.com>
2019-03-13 16:58:52 +0100

    added a page template file

---
+++ page-template-file.php
@@ -0,0 +1,12 @@
+<?php
+  include_once "".$_SERVER['DOCUMENT_ROOT']."/supertorrents/assets/inc/pathVar.inc.php";
+  include_once($headerINC);
+?>
+
+  <main class="main">
+
+  </main>
+
+<?php
+  include_once($footerINC);
+?>
