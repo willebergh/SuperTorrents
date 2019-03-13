36e8043da85aa1ba00d3a5d4fc3a30ce944519b3
willebergh <48175538+willebergh@users.noreply.github.com>
2019-03-13 16:51:39 +0100

    added home dir with index file

---
+++ home/index.php
@@ -0,0 +1,81 @@
+<?php
+  include_once "".$_SERVER['DOCUMENT_ROOT']."/supertorrents/assets/inc/pathVar.inc.php";
+  include_once($dbhINC);
+  include_once($headerINC);
+
+?>
+  <style media="screen">.headerNav_liHome a {background-color: #2d98da;}</style>
+  <main class="main">
+    <div class="frontPageArticleWrapper">
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Torrents right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Movies right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'movies'  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Tv-shows right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'tv-shows'  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Music right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'music'  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Games right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'games'  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+      <div class="frontPageArticle">
+        <div class="tableContainer">
+          <div class="browseTorrent_tableHeader">
+            <h5>Most popular Software right now</h5>
+          </div>
+        </div>
+        <?php
+          $sql = "SELECT uuid, category, title, size, date, uploader, seeders, leechers FROM torrents WHERE category = 'tv-shows'  ORDER BY seeders DESC LIMIT 10";
+          require($browseTorrent_tableINC);
+        ?>
+      </div>
+    </div>
+  </main>
+
+<?php
+  include_once($footerINC);
+?>
