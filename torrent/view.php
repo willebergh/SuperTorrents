623ad829ee64d6993cfb0bdff0ce303cabe394d3
William Bergh <william.bergh1@learnet.se>
2019-03-14 15:25:49 +0100

    added basic html and css to get the styling right on the individual torrent view page

--- torrent/view.php
+++ torrent/view.php
@@ -14,4 +14,53 @@
 
   if ($result->num_rows > 0) {
 
+  } else {
+
   }
+?>
+<main class="main">
+  <div class="torrentViewer">
+    <div class="torrentViewer_title">
+      <h1>title</h1>
+    </div>
+    <div class="torrentViewer_info">
+      <div class="torrentViewer_info_item">
+        <a>Category:</a>
+        <a href="#">Games</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Files:</a>
+        <a href="#">3</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Total Size:</a>
+        <a href="#">12 GB</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Upload Date:</a>
+        <a href="#">2019-03-14</a>
+      </div>
+    </div>
+    <div class="torrentViewer_info">
+      <div class="torrentViewer_info_item">
+        <a>Seeders:</a>
+        <a href="#">324</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Leechers:</a>
+        <a href="#">39</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Downloads:</a>
+        <a href="#">64</a>
+      </div>
+      <div class="torrentViewer_info_item">
+        <a>Uploaded By:</a>
+        <a href="#">BERGET</a>
+      </div>
+    </div>
+    <div class="torrentViewer_description">
+      <p>alksjdlka askdj asa skdjaskljdas asjd kajsdaskdja</p>
+    </div>
+  </div>
+</main>
