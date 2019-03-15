f4046761cf82a10ec5a75f98fd40f356ad8ce0fc
William Bergh <william.bergh1@learnet.se>
2019-03-15 15:17:31 +0100

    added login page styling for mobile

--- user/login.php
+++ user/login.php
@@ -9,11 +9,11 @@
     <form class="form">
       <div class="form_username">
         <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="username" placeholder="username">
       </div>
       <div class="form_password">
         <label for="passowrd">Password</label>
-        <input type="password" name="password" placeholder="Password">
+        <input type="password" name="password" placeholder="password">
       </div>
       <div class="form_extra_links">
         <a href="./signup.php">Forgot password?</a>
