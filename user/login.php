165bda804c63d37b7e3204c5dcb15891f81e2899
William Bergh <william.bergh1@learnet.se>
2019-03-15 21:38:46 +0100

    moved styling from main.css to user.login.css

--- user/login.php
+++ user/login.php
@@ -1,30 +1,29 @@
 <?php
+  $page_uuid_css = "user.login.css";
   include_once "".$_SERVER['DOCUMENT_ROOT']."/supertorrents/assets/inc/pathVar.inc.php";
   include_once($headerINC);
 ?>
 
-<main class="main">
-  <div class="formContainer">
-    <h1>Login</h1>
-    <form class="form">
-      <div class="form_username">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="username">
-      </div>
-      <div class="form_password">
-        <label for="passowrd">Password</label>
-        <input type="password" name="password" placeholder="password">
-      </div>
-      <div class="form_extra_links">
-        <a href="./signup.php">Forgot password?</a>
-        <a href="./signup.php">Signup now!</a>
-      </div>
-      <div class="form_button">
-        <button type="submit" name="login_button">Login</button>
-      </div>
-    </form>
-  </div>
-</main>
+<div class="formContainer">
+  <h1>Login</h1>
+  <form class="form">
+    <div class="form_username">
+      <label for="username">Username</label>
+      <input type="text" name="username" placeholder="username">
+    </div>
+    <div class="form_password">
+      <label for="passowrd">Password</label>
+      <input type="password" name="password" placeholder="password">
+    </div>
+    <div class="form_extra_links">
+      <a href="./signup.php">Forgot password?</a>
+      <a href="./signup.php">Signup now!</a>
+    </div>
+    <div class="form_button">
+      <button type="submit" name="login_button">Login</button>
+    </div>
+  </form>
+</div>
 
 <?php
   include_once($footerINC);
