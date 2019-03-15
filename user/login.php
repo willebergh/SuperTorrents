00802feafd72b8e1bc19e097144e4c0763f0654b
William Bergh <william.bergh1@learnet.se>
2019-03-15 14:18:13 +0100

    removed h3 tag from labels

--- user/login.php
+++ user/login.php
@@ -8,11 +8,11 @@
     <h1>Login</h1>
     <form class="form">
       <div class="form_username">
-        <label for="username"><h3>Username</h3></label>
+        <label for="username">Username</label>
         <input type="text" name="username" placeholder="Username">
       </div>
       <div class="form_password">
-        <label for="passowrd"><h3>Password</h3></label>
+        <label for="passowrd">Password</label>
         <input type="password" name="password" placeholder="Password">
       </div>
       <div class="form_extra_links">
