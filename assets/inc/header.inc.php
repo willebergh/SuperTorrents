edb103573f5efb65dec72dd3d39d2098aca99cc9
William Bergh <william.bergh1@learnet.se>
2019-03-16 21:21:31 +0100

    removed class

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -62,7 +62,7 @@
       <div class="header_searchBarWrapper">
         <form class="header_searchBar" action="/supertorrents/assets/script/search.script.php" method="get">
           <input id="searchBar_input" type="text" name="search" placeholder="Search torrents...">
-          <button type="submit" name="">Go</button>
+          <button type="submit" name=""><span class="fas fa-search"></span></button>
         </form>
       </div>
       <div id="mobile_side_menu" class="mobile-side-menu">
